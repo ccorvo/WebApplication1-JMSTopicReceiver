f5e78a82b8c2a41e226063e0fc52c397b6bcd0aa
unknown <ccorvo@msenb0053.njmrsnet-aegis.com>
2014-01-08 10:37:44 -0500

    Firt Commit of JMS Topic Receiver

---
+++ WebApplication1-JMSTopicReceiver-ejb/src/java/jmsTopicReceiver/ReceiveAlertNotification.java
@@ -0,0 +1,51 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package jmsTopicReceiver;
+
+import javax.ejb.ActivationConfigProperty;
+import javax.ejb.MessageDriven;
+import javax.jms.JMSException;
+import javax.jms.Message;
+import javax.jms.MessageListener;
+import javax.jms.TextMessage;
+
+/**
+ *
+ * @author ccorvo
+ */
+@MessageDriven(mappedName = "jms/NotificationServiceTopic", activationConfig = {
+    @ActivationConfigProperty(propertyName = "acknowledgeMode", propertyValue = "Auto-acknowledge"),
+    @ActivationConfigProperty(propertyName = "destinationType", propertyValue = "javax.jms.Topic"),
+    @ActivationConfigProperty(propertyName = "subscriptionDurability", propertyValue = "Durable"),
+    @ActivationConfigProperty(propertyName = "clientId", propertyValue = "ReceiveAlertNotification"),
+    @ActivationConfigProperty(propertyName = "subscriptionName", propertyValue = "ReceiveAlertNotification")
+})
+public class ReceiveAlertNotification implements MessageListener {
+    
+    public ReceiveAlertNotification() {
+    }
+    
+    @Override
+    public void onMessage(Message message) {
+        
+        TextMessage textMessage = (TextMessage)message;
+        
+        try{
+           String messageContents = textMessage.getText();
+           System.out.println("CORVO AlertNotification MDB Received message: " + messageContents); 
+        }
+        catch(JMSException ex){
+            
+            
+        }
+        
+        
+        
+        
+        
+        
+        
+    }
+}
